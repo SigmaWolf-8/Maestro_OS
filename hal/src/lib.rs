a92f19309a6ff855a704c0b93aba2e3589300d41
SigmaWolf-8 <Capomastro@Outlook.com>
2026-02-07 06:09:14 -0700

    Scaffold: hal/src

---
+++ hal/src/lib.rs
@@ -0,0 +1,10 @@
+//! Maestro Hardware Abstraction Layer
+//! Unified interface for CPU, timer, display, storage, and input devices
+
+#![no_std]
+
+pub mod cpu;
+pub mod timer;
+pub mod display;
+pub mod storage;
+pub mod input;
