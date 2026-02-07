93bd31c44f08f9b0c2761803fa75fb876de640c5
SigmaWolf-8 <Capomastro@Outlook.com>
2026-02-07 06:10:51 -0700

    Scaffold: installer

---
+++ installer/build-iso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ISO Builder for Maestro OS
+# Creates a bootable ISO image for distribution
+# Usage: ./installer/build-iso.sh
+
+set -e
+
+VERSION="0.1.0"
+OUTPUT="maestro-os-.iso"
+
+echo "Building Maestro OS v ISO..."
+echo "TODO: Implement ISO build pipeline"
+echo "Output: "
