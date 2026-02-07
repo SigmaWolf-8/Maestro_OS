9cf56b8a53da55b6f1b36b5566eff1bc15b18585
SigmaWolf-8 <Capomastro@Outlook.com>
2026-02-07 06:09:57 -0700

    Scaffold: desktop

---
+++ desktop/src/themes/mod.rs
@@ -0,0 +1,12 @@
+//! Theme Engine
+//! Dynamic theming with Quantum Obsidian default
+
+#![allow(unused)]
+
+pub struct Theme {
+    pub name: &'static str,
+    pub primary_color: u32,
+    pub background_color: u32,
+    pub surface_color: u32,
+    pub text_color: u32,
+}
